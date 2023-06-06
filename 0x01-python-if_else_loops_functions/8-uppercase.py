9d191fe447d780dcca49e6a282de55a3e05b657a
official-okello <officialokello@gmail.com>
2023-06-07 00:05:03 +0300

    Convert lower to uppercase, print char

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for x in str:
         if ord(x) in range(97, 123):
-            print("{:c}".format(ord(x - 32)), end='')
+            print("{:c}".format(ord(x) - 32), end='')
         else:
             print(x, end='')
