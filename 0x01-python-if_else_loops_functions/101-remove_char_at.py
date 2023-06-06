702b1face2d1a423927b9a8ef8d4facfada18a2b
official-okello <officialokello@gmail.com>
2023-06-07 02:10:59 +0300

    Removes char in position n from string, return inside function

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    if n > len(str) or str == '':
+    if n > len(str) or str == '' or n < 0:
         print(str, end='')
     else:
         for x in range(len(str)):
