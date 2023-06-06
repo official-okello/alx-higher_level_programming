68f39358a7a3160701ad698ea38f3659133be33a
official-okello <officialokello@gmail.com>
2023-06-06 22:46:09 +0300

    Removed one for loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for x in range(10):
-    for y in range(10):
-        if x == 9 and y == 9:
-            print("{}{}".format(x, y))
-        else:
-            print("{}{}".format(x, y), end=', ')
+for x in range(100):
+    if x == 99:
+        print(x)
+    else:
+        print("{:02d}".format(x), end=', ')
