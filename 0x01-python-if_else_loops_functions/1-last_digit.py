844340b015a05a4c5a088ce8c940fd634c7e05d5
official-okello <officialokello@gmail.com>
2023-06-06 20:43:55 +0300

    Find last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,11 @@ number = random.randint(-10000, 10000)
 if number < 0:
     last_digit = number % -10
 else:
-    last_digit - number % 10
+    last_digit = number % 10
 
 print("Last digit of {} is {} ".format(number, last_digit), end='')
 
-if last-digit > 5:
+if last_digit > 5:
     print("and is greater than 5")
 elif last_digit < 6:
     if last_digit == 0:
