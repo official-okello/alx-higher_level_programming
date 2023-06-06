0f86acb9c7afe0cbab60f6091e977ed05a95842e
official-okello <officialokello@gmail.com>
2023-06-07 00:39:19 +0300

    Prints last digit of a number, solved for negative numbers

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,8 @@
 
 def print_last_digit(number):
     if number < 0:
-        last_digit *= number % -10
+        last_digit = number % -10
+		last_digit *= -1
     else:
         last_digit = number % 10
     print("{}".format(last_digit), end='')
