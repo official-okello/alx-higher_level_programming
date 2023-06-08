ca652573865c9d091c01c3441aa07c54dd290be4
official-okello <officialokello@gmail.com>
2023-06-08 15:16:26 +0300

    Performs calculations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,13 +3,14 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+
     if len(sys.argv) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
         op = sys.argv[2]
-        a = sys.argv[1]
-        b = sys.argv[3]
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
 
         if op == '+':
             print("{} + {} = {}".format(a, b, add(a, b)))
