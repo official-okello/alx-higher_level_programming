660ed78334f74178bd4d51070907948a84521ed6
official-okello <officialokello@gmail.com>
2023-06-08 14:13:35 +0300

    Number od args using sys,argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     length = len(sys.argv)
 
     if i == 1:
-        print("{} arguments.".format(length))
+        print("{} arguments.".format(length -1))
     elif i == 2:
-        print("{} argument:".format(length))
+        print("{} argument:".format(length - 1))
     else:
-        print("{} arguments:".format(length))
+        print("{} arguments:".format(length - 1))
 
     for i in range(1, length):
         print('{:d}: {:s}'.format(i, sys.argv[i]))
