76a541f62515741165829010f8a55ac7958e6580
official-okello <officialokello@gmail.com>
2023-06-12 21:08:36 +0300

    Fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        for i in range 2:
-            if tuple[i] = '':
-                tuple_a[i] = 0
-    if len(tuple_b) < 2:
-        for j in range 2:
-            if tuple_b[i] = '':
-                tuple_b[i] = 0
-    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-	return (new_tuple)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
