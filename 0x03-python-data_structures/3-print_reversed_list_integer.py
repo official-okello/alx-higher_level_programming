ab5c0a57ab03e2e8623d968ede2d0dab350570d9
official-okello <officialokello@gmail.com>
2023-06-12 20:32:36 +0300

    Fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        pass
+        return[my_list]
     for i in range(1, len(my_list) + 1):
         print("{:d}".format(my_list[-i]))
