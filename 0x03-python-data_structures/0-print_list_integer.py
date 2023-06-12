b049a308a91630f3e2793d39ccb5cc74902895de
official-okello <officialokello@gmail.com>
2023-06-12 19:32:35 +0300

    Fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".fo`rmat(num))
+    for x in range(len(my_list)):
+        print("{}".fo`rmat(my_list[x]))
