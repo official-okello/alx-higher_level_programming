fc015cedcfba22c4179ec60ef8070fb1cda7a8a9
official-okello <officialokello@gmail.com>
2023-06-12 19:50:01 +0300

    Fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,8 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    if idx > (len(my_list) - 1):
+
+    l = len(my_list)
+    if idx > l - 1:
         return (None)
     return (my_list(idx])
