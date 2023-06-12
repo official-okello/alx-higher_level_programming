1eea3652993a2bd5ec11f0f23f14bd848c59fdbb
official-okello <officialokello@gmail.com>
2023-06-12 20:34:14 +0300

    Fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         new_list = my_list
         return (new_list)
-    else:
-        my_list[idx] = element
-        return (my_list)
+
+     my_list[idx] = element
+     return (my_list)
