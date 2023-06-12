e66cb6847e1cce231437a2ee425ca709723b45d6
official-okello <officialokello@gmail.com>
2023-06-12 21:00:07 +0300

    Fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = []
-    for i in len(my_string):
-        if my_string[i] == "c" or if my_string[i] == "C":
-            new_string[i] = ''
-        new_string[i] = my_string[i]
+    new_string = ''
+    for i in my_string:
+        if i != "c" and i != "C":
+            new_string += i
     return (new_string)
