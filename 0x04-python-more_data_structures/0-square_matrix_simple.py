615c179b9cf4d0af89fb6511d14c1002de285953
official-okello <officialokello@gmail.com>
2023-06-14 22:14:06 +0300

    Fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 
 def square_matrix_simple(matrix=[]):
     n_mat = matrix.copy()
-
-    for i in range(len(matrix)):
-    n_mat[i] = list(map(lambda x: x **2, matrix[i]))
-    return (n_mat)
+    for i in matrix:
+        new_matrix.append(list(map((lambda x: x ** 2), i)))
+    return new_matrix
