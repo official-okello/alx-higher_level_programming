c22952ea9100b5e005814c829174f7bf6a174903
official-okello <officialokello@gmail.com>
2023-06-14 22:23:20 +0300

    Fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    return (list(map(lambda x: x if x != search else replace, my_list )))
+    n_list = list(map(lambda x: x if x != search else replace, my_list))
+    return (n_list)
