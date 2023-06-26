c3a270dc44330cb32a69ce8a642a99a308ed3a7f
official-okello <officialokello@gmail.com>
2023-06-26 19:52:36 +0300

    Quick fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,9 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print(my_list[i], end="")
             i += 1
+        print()
         return i
     except:
+        print()
         return i
     print("\n")
