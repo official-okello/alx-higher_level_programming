40745c507fdb6a6421fa425dfca43b9649e49d14
official-okello <officialokello@gmail.com>
2023-06-26 20:12:38 +0300

    Pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
-        except Exception:
+        except (TypeError, ValueError):
             continue
     print()
     return (i)
