6db772dde47392c07fc2c4884383c28a383b8409
official-okello <officialokello@gmail.com>
2023-06-26 20:19:51 +0300

    Pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,8 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         except ValueError:
             pass
-        except Exception:
-            pass
         else:
             count += 1
 
