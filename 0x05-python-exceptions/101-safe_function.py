696307f0cc895059ea4796cecff8d0d0f4ccff69
official-okello <officialokello@gmail.com>
2023-06-26 20:08:24 +0300

    Pycodestyle

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import sys
+
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
-        except (TypeError, ValueError):
+        except Exception:
             continue
     print()
     return (i)
