713b4ef127d51e69bface3dbc2850be9c5cd0381
official-okello <officialokello@gmail.com>
2023-07-03 22:47:25 +0300

    Quick fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -50,6 +50,6 @@ class Rectangle:
 
     def perimeter(self):
         """Method that returns the perimeter of a rectangle"""
-        if self.__height == 0 and self.__width == 0:
+        if self.__height == 0 or self.__width == 0:
             return 0
         return (2 * self.__height) + (2 * self.__width)
