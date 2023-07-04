270aa10a526fb478c971d7cdd34498610021b789
official-okello <officialokello@gmail.com>
2023-07-04 11:09:50 +0300

    It all works

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""101-lazy_matrix_mul Module"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function that multiplies 2 matrices"""
+    return np.matmul(m_a, m_b)
