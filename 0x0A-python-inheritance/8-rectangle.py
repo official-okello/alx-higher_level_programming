3fdccdfff874c4e8cca83f9579fe9de21358323a
official-okello <officialokello@gmail.com>
2023-07-10 20:43:27 +0300

    Quick fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Main container """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -7,6 +10,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """ Initializes instance """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
