03167a95379da3bba5ffa0c7f4d86ba2e130632f
official-okello <officialokello@gmail.com>
2023-07-10 20:49:09 +0300

    Quick fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+""" Main container """
+
+
 class MyInt(int):
     """ Class that inherits from Class Int"""
 
     def __eq__(self, other):
         """ Method that returns != check """
+
         return int.__ne__(self, other)
 
     def __ne__(self, other):
         """ Method that returns == check """
+
         return int.__eq__(self, other)
