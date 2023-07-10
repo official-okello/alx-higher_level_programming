edd4474d7732024aa304152e71b6fa60499e3c57
official-okello <officialokello@gmail.com>
2023-07-10 20:34:17 +0300

    Quick fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" Main container """
+
+
 def inherits_from(obj, a_class):
+    """ Defines the class """
+
     if type(obj) is a_class:
         return False
     return isinstance(obj, a_class)
