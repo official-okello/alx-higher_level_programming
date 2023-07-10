f0d1b86c46d0771d1c13edb3691dcc71b41c3605
official-okello <officialokello@gmail.com>
2023-07-10 20:32:59 +0300

    Quick fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Main container """
+
+
 def is_kind_of_class(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
+    """ Function that returns True/False if obj is an instance of a_class """
     return isinstance(obj, a_class)
