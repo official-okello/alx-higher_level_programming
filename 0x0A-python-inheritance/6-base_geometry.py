41f8529475f1a38592a6b7be6f2b61701085ad54
official-okello <officialokello@gmail.com>
2023-07-10 20:37:13 +0300

    Quick fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" Main container """
+
+
 class BaseGeometry:
     """ BaseGeometry class """
+
     def area(self):
+	    """ Raises error """
+
         raise Exception("area() is not implemented")
