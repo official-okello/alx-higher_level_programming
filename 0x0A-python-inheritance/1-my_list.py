523e2b819aef240a99c9cb8441e737729e5f1297
official-okello <officialokello@gmail.com>
2023-07-10 20:19:23 +0300

    Quick fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,10 +3,11 @@
 
 
 class MyList(list):
-    """ Class that inherits the attributes references of class list
+    """ Class that inherits the attributes references of class list """
 
     def print_sorted(self):
         """ Method that prints the sorted list """
+
         l_sorted = self.copy()
         l_sorted.sort()
         print(l_sorted)
