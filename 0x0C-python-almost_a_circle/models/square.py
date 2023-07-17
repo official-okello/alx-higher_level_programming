95a419bd9230e76285b91868d7bece4363fe29b5
official-okello <officialokello@gmail.com>
2023-07-17 14:20:47 +0300

    Done with the square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Square module """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes Square class """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ returns square in format """
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                self.y, self.size))
+
+	@property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """ assigns attributes """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ returns dictionary represantation of a square """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
