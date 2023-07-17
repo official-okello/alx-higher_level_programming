e32c7ef40811e5d944467fab37128ce86c6530d7
official-okello <officialokello@gmail.com>
2023-07-17 21:49:10 +0300

    quick fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,139 +1,73 @@
 #!/usr/bin/python3
-""" Main Class """
-
+"""Class Module"""
 import json
-import os.path
-import csv
 
 
 class Base:
-    """ Base Class """
-
+    """ base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Class constructor """
-
+        """initiation method"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    def integer_validator(self, name, value):
+        """check if value is an integer"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be > 0'.format(name))
+
+    def integer_validator2(self, name, value):
+        """check if value is an integer"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value < 0:
+            raise ValueError('{} must be >= 0'.format(name))
+
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ returns JSON representation of list_dictionaries """
-        if list_dictionaries is None or list_dictionaries == "[]":
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """ writes JSON representation of list_objs to a file """
-        filename = "{}.json".format(cls.__name__)
-        list_dic = []
-
-        if not list_objs:
-            pass
-        else:
-            for i in range(len(list_objs)):
-                list_dic.append(list_objs[i].to_dictionary())
-
-        lists = cls.to_json_string(list_dic)
-
-        with open(filename, 'w') as f:
-            f.write(lists)
+        """returns JSON string"""
+        return json.dumps(list_dictionaries or [])
 
     @staticmethod
     def from_json_string(json_string):
-        """ returns the list of the JSON string representation """
-        if not json_string:
-            return []
-        return json.loads(json_string)
+        """json to string static method"""
+        if json_string:
+            return json.loads(json_string)
+        return []
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string to a file"""
+        if list_objs:
+            j = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        else:
+            j = '[]'
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(j)
 
     @classmethod
     def create(cls, **dictionary):
-        """ returns an instance with all attributes already set """
+        """Returns instance with all attributes set"""
         if cls.__name__ == "Rectangle":
-            dummy = cls(15, 15)
-        else:
-            dummy = cls(15)
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
     @classmethod
     def load_from_file(cls):
-        """ returns a list of instances """
-        filename = "{}.json".format(cls.__name__)
-
-        if os.path.exists(filename) is False:
+        """Returns a list of instances"""
+        try:
+            filename = cls.__name__ + '.json'
+            with open(filename, mode='r') as f:
+                d = cls.from_json_string(f.read())
+            return [cls.create(**x) for x in d]
+        except FileNotFoundError:
             return []
-
-        with open(filename, 'r') as f:
-            list_str = f.read()
-
-        list_cls = cls.from_json_string(list_str)
-        list_ins = []
-
-        for index in range(len(list_cls)):
-            list_ins.append(cls.create(**list_cls[index]))
-
-        return list_ins
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """ serializes in CSV """
-        filename = "{}.csv".format(cls.__name__)
-
-        if cls.__name__ == "Rectangle":
-            list_dic = [0, 0, 0, 0, 0]
-            list_keys = ['id', 'width', 'height', 'x', 'y']
-        else:
-            list_dic = ['0', '0', '0', '0']
-            list_keys = ['id', 'size', 'x', 'y']
-
-        matrix = []
-
-        if not list_objs:
-            pass
-        else:
-            for obj in list_objs:
-                for kv in range(len(list_keys)):
-                    list_dic[kv] = obj.to_dictionary()[list_keys[kv]]
-                matrix.append(list_dic[:])
-
-        with open(filename, 'w') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerows(matrix)
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """ deserializes from CSV """
-        filename = "{}.csv".format(cls.__name__)
-
-        if os.path.exists(filename) is False:
-            return []
-
-        with open(filename, 'r') as readFile:
-            reader = csv.reader(readFile)
-            csv_list = list(reader)
-
-        if cls.__name__ == "Rectangle":
-            list_keys = ['id', 'width', 'height', 'x', 'y']
-        else:
-            list_keys = ['id', 'size', 'x', 'y']
-
-        matrix = []
-
-        for csv_elem in csv_list:
-            dict_csv = {}
-            for kv in enumerate(csv_elem):
-                dict_csv[list_keys[kv[0]]] = int(kv[1])
-            matrix.append(dict_csv)
-
-        list_ins = []
-
-        for index in range(len(matrix)):
-            list_ins.append(cls.create(**matrix[index]))
-
-        return list_ins
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,73 +1,46 @@
 #!/usr/bin/python3
-"""Class Module"""
-import json
+"""Square Module"""
+from models.rectangle import Rectangle
 
 
-class Base:
-    """ base class"""
-    __nb_objects = 0
+class Square(Rectangle):
+    """Sqare subclass"""
 
-    def __init__(self, id=None):
-        """initiation method"""
-        if id is not None:
-            self.id = id
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """print method"""
+        return ("[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+                                              self.id, self.x,
+                                              self.y, self.width))
+
+    @property
+    def size(self):
+        """width getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """width and height setter"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """update square"""
+
+        if args:
+            i = 0
+            listme = ['id', 'size', 'x', 'y']
+            for arg in args:
+                setattr(self, listme[i], arg)
+                i += 1
+            return
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
-    def integer_validator(self, name, value):
-        """check if value is an integer"""
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be > 0'.format(name))
-
-    def integer_validator2(self, name, value):
-        """check if value is an integer"""
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value < 0:
-            raise ValueError('{} must be >= 0'.format(name))
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """returns JSON string"""
-        return json.dumps(list_dictionaries or [])
-
-    @staticmethod
-    def from_json_string(json_string):
-        """json to string static method"""
-        if json_string:
-            return json.loads(json_string)
-        return []
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """writes JSON string to a file"""
-        if list_objs:
-            j = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
-        else:
-            j = '[]'
-        with open(cls.__name__ + '.json', 'w') as f:
-            f.write(j)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Returns instance with all attributes set"""
-        if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        if cls.__name__ == "Square":
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
-
-    @classmethod
-    def load_from_file(cls):
-        """Returns a list of instances"""
-        try:
-            filename = cls.__name__ + '.json'
-            with open(filename, mode='r') as f:
-                d = cls.from_json_string(f.read())
-            return [cls.create(**x) for x in d]
-        except FileNotFoundError:
-            return []
+    def to_dictionary(self):
+        """returns a dictionary of Square"""
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
