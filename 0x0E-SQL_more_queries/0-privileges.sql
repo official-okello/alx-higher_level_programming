729ca7b07d6b68a11bdc27bebb9ee6ac82ce80ac
official-okello <officialokello@gmail.com>
2023-08-16 21:28:31 +0300

    Corrections

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Script that lists all privileges of the MySQL users
--- user_0d_1 and user_0d_2 on your server (in localhost).
+-- user_0d_1 and user_0d_2 on a server (in localhost).
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,11 +2,6 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- flushes privileges after creating a user or granting a user permissions
-
-FLUSH PRIVILEGES;
-
 -- grants a user all permissions in root
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
